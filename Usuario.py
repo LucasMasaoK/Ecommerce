148e7dcdb3093d8d283a11caeddc7fbe7c99776c
tiramt <128667467+tiramt@users.noreply.github.com>
2023-11-08 20:39:12 -0300

    Update Usuario.py

--- Usuario.py
+++ Usuario.py
@@ -1,13 +1,31 @@
+from prettytable import PrettyTable
+usuarios = {"thiago":[3112]}
 def login():
     sair=False
     while sair!=True:
+        print(" Bem vindo ao sistema de usuário ")
+        print('1 - Faça o Login')
+        print('2 - Criar Usuário')
+        op=int(input('Digite a opção escolhida:'))
         email=input('Digite seu email:')
         senha=input('Digite sua senha:')
-        if email=='admin' and senha=='admin':
-            print('Usuario Logado!\n')
-            return True
-        else:
+        if op ==1:
+            if email == 'admin' and senha == 'admin':
+                print('Usuario Logado!\n')
+                return True
+            if email in usuarios:
+                if senha == usuarios[email][0]:
+                    print("Login bem-sucedido!")
+                    return True
             print('Usuario ou senha Incorretos\n')
-            continue
+        if op ==2:
+            usuarios[email] = [senha]
+            print("Usuário criado com sucesso!")
+            return True
 
 
+def criar_usuario():
+    email = input("Digite o nome de usuário: ")
+    senha = input("Digite a senha: ")
+    usuarios[email] = [senha]
+    print("Usuário criado com sucesso!")
